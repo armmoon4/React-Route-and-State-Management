be824229562ed81bed8087bf90b17e539ce4d2c5
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-27 21:05:37 +0600

    update

---
+++ M47_Book vibe/boipoka/src/pages/Books/Books.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Books = () => {
+    const [allbooks , setAllBooks] = useState([]); 
+    return (
+        <div>
+            <h2>helllo i am books</h2>
+        </div>
+    );
+};
+// 47-3   duration 9
+export default Books;
