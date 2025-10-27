9a745dd5034aa4549cc6e5ec9cdceed65fdd035a
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-27 22:57:36 +0600

    update

--- M47_Book vibe/boipoka/src/pages/Books/Books.jsx
+++ M47_Book vibe/boipoka/src/pages/Books/Books.jsx
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Books = () => {
     const [allbooks , setAllBooks] = useState([]); 
+
+    useEffect(()=> {
+        fetch('booksData.json')
+        .then(res => res.json()
+        .then(data => {
+           setAllBooks(data);
+        })
+    )
+    } ,[])
     return (
         <div>
             <h2>helllo i am books</h2>
         </div>
     );
 };
-// 47-3   duration 9
+// 47-3   done 9
 export default Books;
