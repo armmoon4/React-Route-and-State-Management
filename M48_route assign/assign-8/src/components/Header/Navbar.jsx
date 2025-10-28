c7550631444384800574a6ed1a7bc404ebbb13a3
armmoon4 <rahman15-4343@diu.edu.bd>
2025-10-28 19:50:41 +0600

    navbar done

---
+++ M48_route assign/assign-8/src/components/Header/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import navLogo from '../../assets/logo.png'
+const Navbar = () => {
+    const link = <>
+        <li className='m-2 hover:cursor-pointer'>Home</li>
+        <li className='m-2 hover:cursor-pointer'>My-Bookings</li>
+        <li className='m-2 hover:cursor-pointer'>Blogs</li>
+        <li className='m-2 hover:cursor-pointer'>Contact Us</li>
+    </>
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        {link}
+                    </ul>
+                </div>
+                <img className='w-[30px]' src={navLogo} alt="" />
+                <a className="btn btn-ghost text-xl">Phudo</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                   {link}
+                </ul> 
+            </div>
+            <div className="navbar-end">
+                <a className="btn rounded-3xl bg-blue-600">Emergency</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
